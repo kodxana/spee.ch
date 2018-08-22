6cd734cca1750df0d5f481bec0e07893a521ff2c
Travis Eden <daovist@gmail.com>
2018-08-22 14:21:45 -0400

    use shortId in edit link

--- client/src/containers/AssetTitle/index.js
+++ client/src/containers/AssetTitle/index.js
@@ -6,12 +6,14 @@ const mapStateToProps = (props) => {
   const { claimData: { title } } = selectAsset(props.show);
   const requestId = props.show.request.id;
   const channelName = props.channel.loggedInChannel.name;
+  const {shortId} = props.channel.loggedInChannel;
   const editable = channelName && requestId && requestId.split('#')[3] === channelName;
   return {
     title,
     editable,
     channel: editable ? channelName : undefined,
     name   : editable ? requestId.split('#')[1] : undefined,
+    shortId,
   };
 };
 
--- client/src/containers/AssetTitle/view.jsx
+++ client/src/containers/AssetTitle/view.jsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import Row from '@components/Row';
 
-const AssetTitle = ({ title, editable, channel, name }) => {
+const AssetTitle = ({ title, editable, channel, name, shortId }) => {
   return (
     <Row>
       <h3>
         {title}
-        {editable && (<span> (<a href={`/edit/${name}`}>edit</a>)</span>)}
+        {editable && (<span> (<a href={`/edit/${channel}:${shortId}/${name}`}>edit</a>)</span>)}
       </h3>
     </Row>
   );
