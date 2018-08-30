cee3436d7171d55e84c45ec309c603eceb00447c
Travis Eden <daovist@gmail.com>
2018-08-30 09:51:21 -0400

    one big try-catch

--- server/controllers/api/claim/publish/publish.js
+++ server/controllers/api/claim/publish/publish.js
@@ -1,6 +1,6 @@
 const logger = require('winston');
-const { publishClaim } = require('../../../../lbrynet');
 const db = require('../../../../models');
+const { publishClaim } = require('../../../../lbrynet');
 const { createFileRecordDataAfterPublish } = require('../../../../models/utils/createFileRecordData.js');
 const { createClaimRecordDataAfterPublish } = require('../../../../models/utils/createClaimRecordData.js');
 const deleteFile = require('./deleteFile.js');
@@ -17,89 +17,52 @@ const publish = async (publishParams, fileName, fileType) => {
     file,
     claim;
 
-  const handleError = async (err) => {
-    logger.error('PUBLISH ERROR', err);
-    try {
-      await deleteFile(filePath);
-    } catch(error) {
-      logger.error(error);
-    }
-    return err;
-  };
-
   try {
     publishResults = await publishClaim(publishParams);
     logger.info(`Successfully published ${publishParams.name} ${fileName}`, publishResults);
-  }
-  catch(error) {
-    logger.info('pub failed', publishParams);
-    return handleError(error);
-  }
 
-  // get the channel information
-  if (publishParams.channel_name) {
-    logger.debug(`this claim was published in channel: ${publishParams.channel_name}`);
-    try {
+    // get the channel information
+    if (publishParams.channel_name) {
+      logger.debug(`this claim was published in channel: ${publishParams.channel_name}`);
       channel = await db.Channel.findOne({
         where: {
           channelName: publishParams.channel_name,
         },
       });
+    } else {
+      logger.debug('this claim was not published in a channel');
+      channel = null;
     }
-    catch(error) {
-      return handleError(error);
-    }
-  } else {
-    logger.debug('this claim was not published in a channel');
-    channel = null;
-  }
-  certificateId = channel ? channel.channelClaimId : null;
-  channelName = channel ? channel.channelName : null;
-  logger.info(`certificateId: ${certificateId}`);
+    certificateId = channel ? channel.channelClaimId : null;
+    channelName = channel ? channel.channelName : null;
+    logger.debug(`certificateId: ${certificateId}`);
 
-  const {name} = publishParams;
-  const {claim_id: claimId} = publishResults;
-  const upsertCriteria = {
-    name,
-    claimId,
-  };
+    const upsertCriteria = {name: publishParams.name, claimId: publishParams.claim_id};
 
-  try {
-    [ fileRecord, claimRecord ] = await Promise.all([
+    [fileRecord, claimRecord] = await Promise.all([
       newFile ? createFileRecordDataAfterPublish(fileName, fileType, publishParams, publishResults) : db.File.findOne({where: {claimId}}),
       createClaimRecordDataAfterPublish(certificateId, channelName, fileName, fileType, publishParams, publishResults),
     ]);
-  }
-  catch(error) {
-    return handleError(error);
-  }
 
-  logger.info('fileRecord', fileRecord);
-  logger.info('claimRecord', claimRecord);
-
-  try {
-    [ file, claim ] = await Promise.all([
+    [file, claim] = await Promise.all([
       db.upsert(db.File, fileRecord, upsertCriteria, 'File'),
       db.upsert(db.Claim, claimRecord, upsertCriteria, 'Claim'),
     ]);
-    // file = await db.upsert(db.File, fileRecord, upsertCriteria, 'File');
-    // claim = await db.upsert(db.Claim, claimRecord, upsertCriteria, 'Claim');
-    logger.info(`${newFile ? 'File and ' : ''}Claim records successfully created`);
-  }
-  catch(error) {
-    return handleError(error);
+    logger.debug('File and Claim records successfully created');
+
+    await Promise.all([
+      file.setClaim(claim),
+      claim.setFile(file),
+    ]);
+    logger.debug('File and Claim records successfully associated');
+
+    return publishResults;
+  } catch (err) {
+    logger.error('PUBLISH ERROR', err);
+    await deleteFile(filePath);
+    return err;
   }
 
-  try {
-    file.setClaim(claim);
-    claim.setFile(file);
-    logger.info('File and Claim records successfully associated');
-  }
-  catch(error) {
-    return handleError(error);
-  }
-
-  return publishResults;
 };
 
 module.exports = publish;
