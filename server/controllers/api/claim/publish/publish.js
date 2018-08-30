171773f271e10a8832bf9baa338a3b376e49d713
Travis Eden <daovist@gmail.com>
2018-08-29 22:47:26 -0400

    fixed everything

--- server/controllers/api/claim/publish/publish.js
+++ server/controllers/api/claim/publish/publish.js
@@ -5,77 +5,101 @@ const { createFileRecordDataAfterPublish } = require('../../../../models/utils/c
 const { createClaimRecordDataAfterPublish } = require('../../../../models/utils/createClaimRecordData.js');
 const deleteFile = require('./deleteFile.js');
 
-const publish = (publishParams, fileName, fileType) => {
-  return new Promise((resolve, reject) => {
-    let publishResults, certificateId, channelName;
-    logger.info('publishClaim:', publishParams);
-    // publish the file
-    return publishClaim(publishParams)
-      .then(tx => {
-        logger.info(`Successfully published ${publishParams.name} ${fileName}`, tx);
-        publishResults = tx;
-        // get the channel information
-        if (publishParams.channel_name) {
-          logger.debug(`this claim was published in channel: ${publishParams.channel_name}`);
-          return db.Channel.findOne({
-            where: {
-              channelName: publishParams.channel_name,
-            },
-          });
-        } else {
-          logger.debug('this claim was not published in a channel');
-          return null;
-        }
-      })
-      .then(channel => {
-        // set channel information
-        certificateId = null;
-        channelName = null;
-        if (channel) {
-          certificateId = channel.channelClaimId;
-          channelName = channel.channelName;
-        }
-        logger.debug(`certificateId: ${certificateId}`);
-      })
-      .then(() => {
-        return Promise.all([
-          createFileRecordDataAfterPublish(fileName, fileType, publishParams, publishResults),
-          createClaimRecordDataAfterPublish(certificateId, channelName, fileName, fileType, publishParams, publishResults),
-        ]);
-      })
-      .then(([fileRecord, claimRecord]) => {
-        logger.info('fileRecord', fileRecord);
-        logger.info('claimRecord', claimRecord);
-        // upsert the records
-        const {name} = publishParams;
-        const {claim_id: claimId} = publishResults;
-        const upsertCriteria = {
-          name,
-          claimId,
-        };
-        return Promise.all([
-          db.upsert(db.File, fileRecord, upsertCriteria, 'File'),
-          db.upsert(db.Claim, claimRecord, upsertCriteria, 'Claim'),
-        ]);
-      })
-      .then(([file, claim]) => {
-        logger.info('File and Claim records successfully created');
-        return Promise.all([
-          file.setClaim(claim),
-          claim.setFile(file),
-        ]);
-      })
-      .then(() => {
-        logger.info('File and Claim records successfully associated');
-        // resolve the promise with the result from lbryApi publishClaim;
-        resolve(publishResults);
-      })
-      .catch(error => {
-        logger.error('PUBLISH ERROR', error);
-        deleteFile(publishParams.file_path); // delete the local file
-        reject(error);
+const publish = async (publishParams, fileName, fileType) => {
+  let publishResults,
+    certificateId,
+    channel,
+    channelName,
+    filePath = publishParams.file_path,
+    newFile = Boolean(filePath),
+    fileRecord,
+    claimRecord,
+    file,
+    claim;
+
+  const handleError = async (err) => {
+    logger.error('PUBLISH ERROR', err);
+    try {
+      await deleteFile(filePath);
+    } catch(error) {
+      logger.error(error);
+    }
+    return err;
+  };
+
+  try {
+    publishResults = await publishClaim(publishParams);
+    logger.info(`Successfully published ${publishParams.name} ${fileName}`, publishResults);
+  }
+  catch(error) {
+    logger.info('pub failed', publishParams);
+    return handleError(error);
+  }
+
+  // get the channel information
+  if (publishParams.channel_name) {
+    logger.debug(`this claim was published in channel: ${publishParams.channel_name}`);
+    try {
+      channel = await db.Channel.findOne({
+        where: {
+          channelName: publishParams.channel_name,
+        },
       });
-  });
+    }
+    catch(error) {
+      return handleError(error);
+    }
+  } else {
+    logger.debug('this claim was not published in a channel');
+    channel = null;
+  }
+  certificateId = channel ? channel.channelClaimId : null;
+  channelName = channel ? channel.channelName : null;
+  logger.info(`certificateId: ${certificateId}`);
+
+  const {name} = publishParams;
+  const {claim_id: claimId} = publishResults;
+  const upsertCriteria = {
+    name,
+    claimId,
+  };
+
+  try {
+    [ fileRecord, claimRecord ] = await Promise.all([
+      newFile ? createFileRecordDataAfterPublish(fileName, fileType, publishParams, publishResults) : db.File.findOne({where: {claimId}}),
+      createClaimRecordDataAfterPublish(certificateId, channelName, fileName, fileType, publishParams, publishResults),
+    ]);
+  }
+  catch(error) {
+    return handleError(error);
+  }
+
+  logger.info('fileRecord', fileRecord);
+  logger.info('claimRecord', claimRecord);
+
+  try {
+    [ file, claim ] = await Promise.all([
+      db.upsert(db.File, fileRecord, upsertCriteria, 'File'),
+      db.upsert(db.Claim, claimRecord, upsertCriteria, 'Claim'),
+    ]);
+    // file = await db.upsert(db.File, fileRecord, upsertCriteria, 'File');
+    // claim = await db.upsert(db.Claim, claimRecord, upsertCriteria, 'Claim');
+    logger.info(`${newFile ? 'File and ' : ''}Claim records successfully created`);
+  }
+  catch(error) {
+    return handleError(error);
+  }
+
+  try {
+    file.setClaim(claim);
+    claim.setFile(file);
+    logger.info('File and Claim records successfully associated');
+  }
+  catch(error) {
+    return handleError(error);
+  }
+
+  return publishResults;
 };
 
 module.exports = publish;
--- server/controllers/api/claim/update/index.js
+++ server/controllers/api/claim/update/index.js
@@ -3,7 +3,7 @@ const logger = require('winston');
 const db = require('../../../../models');
 
 const { details, publishing: { disabled, disabledMessage, thumbnailChannel, thumbnailChannelId, primaryClaimAddress } } = require('@config/siteConfig');
-const { getClaim } = require('../../../../lbrynet');
+const { resolveUri } = require('../../../../lbrynet');
 
 const { sendGATimingEvent } = require('../../../../utils/googleAnalytics.js');
 
@@ -71,22 +71,19 @@ const claimUpdate = ({ body, files, headers, ip, originalUrl, user, tor }, res) 
   // check channel authorization
   authenticateUser(channelName, channelId, channelPassword, user)
     .then(({ channelName, channelClaimId }) => {
-      logger.info('auth good', channelName);
-      // const {name, nsfw, license, title, description} = body;
-      // return Promise.all([
-      //   db.Claim.findOne({ where: { name, channelName } }),
-      //   updateMetadata({nsfw, license, title, description}),
-      //   db.File.findOne({ where: { name } }),
-      //   // parseUpdateFile(files),
-      //   // parseUpdateThumbnail(files, name, license, nsfw),
-      // ]);
-      return db.Claim.findOne({where: {name, channelName,},});
+      return db.Claim.findOne({
+        where: {
+          name,
+          channelName,
+        },
+      });
     })
     .then(claim => {
       claimRecord = claim;
-      return getClaim(`${claim.name}#${claim.claimId}`);
+      return resolveUri(`${claim.name}#${claim.claimId}`);
     })
     .then(fullClaim => {
+      logger.info('fullClaim', fullClaim);
       const metadata = Object.assign({}, {
         title      : claimRecord.title,
         description: claimRecord.description,
@@ -106,80 +103,27 @@ const claimUpdate = ({ body, files, headers, ip, originalUrl, user, tor }, res) 
       if (files.file) {
         publishParams['file_path'] = filePath;
       } else {
-        publishParams['sources'] = {'lbry_sd_hash': fullClaim.stream_hash};
+        fileName = fullClaim.file_name;
+        fileType = fullClaim.mime_type;
+        publishParams['sources'] = fullClaim.claim.value.stream.source;
       }
       return publish(publishParams, fileName, fileType);
     })
-    // .then(([existingClaim, metadataUpdate, existingFile, /*newFile, newThumbnail*/]) => {
-    //   logger.info("multi-promise good");
-    //   if (existingClaim) {
-    //     logger.info('existingClaim', existingClaim);
-    //     return getClaim(`${existingClaim.name}#${existingClaim.claimId}`);
-    //   }
-    //   const metadata = Object.assign({}, {
-    //     title      : existingClaim.title,
-    //     description: existingClaim.description,
-    //     nsfw       : existingClaim.nsfw,
-    //     license    : existingClaim.license,
-    //     language   : 'en',
-    //     author     : details.title,
-    //   }, metadataUpdate);
-    //   if (existingClaim.contentType === 'video/mp4' && files.thumbnail) {
-    //     metadata['thumbnail'] = `${details.host}/${thumbnailChannel}:${thumbnailChannelId}/${name}-thumb`;
-    //     thumbnailParams = {
-    //       name         : `${name}-thumb`,
-    //       file_path    : thumbnailFilePath,
-    //       bid          : 0.01,
-    //       claim_address: primaryClaimAddress,
-    //       channel_name : thumbnailChannel,
-    //       channel_id   : thumbnailChannelId,
-    //       metadata     : {
-    //         title      : `${name} thumbnail`,
-    //         description: `a thumbnail for ${name}`,
-    //         author     : details.title,
-    //         language   : 'en',
-    //         license    : metadata.license,
-    //         nsfw       : metadata.nsfw,
-    //       },
-    //     };
-    //   }
-    //   publishParams = {
-    //     name         : existingClaim.name,
-    //     bid          : 0.01,
-    //     claim_address: primaryClaimAddress,
-    //     channel_name : existingClaim.channelName,
-    //     channel_id   : existingClaim.certificateId,
-    //     metadata,
-    //   };
-    //   if (existingClaim) {
-    //     publishParams['sources'] = {};
-    //   }
-    //   if (files.file) {
-    //     publishParams['file_path'] = filePath;
-    //   }
-    //   if (thumbnailParams) {
-    //     logger.info('doing a thumbnail publish update', thumbnailParams);
-    //     publish(thumbnailParams, thumbnailFileName, thumbnailFileType);
-    //   }
-    //   logger.info('doing a publish update', publishParams);
-    //   return publish(publishParams, fileName, fileType);
-    // })
     .then(result => {
       res.status(200).json({
         success: true,
-        message: 'see below',
-        result,
-        // data   : {
-        //   name    : result.name,
-        //   claimId : result.claim_id,
-        //   url     : `${details.host}/${result.claim_id}/${name}`, // for backwards compatability with app
-        //   showUrl : `${details.host}/${result.claim_id}/${name}`,
-        //   // serveUrl: `${details.host}/${result.claim_id}/${name}${fileE}`,
-        //   lbryTx  : result,
-        // },
+        message: 'update successful',
+        data   : {
+          name    : result.name,
+          claimId : result.claim_id,
+          url     : `${details.host}/${result.claim_id}/${name}`, // for backwards compatability with app
+          showUrl : `${details.host}/${result.claim_id}/${name}`,
+          // serveUrl: `${details.host}/${result.claim_id}/${name}${fileE}`,
+          lbryTx  : result,
+        },
       });
       // record the publish end time and send to google analytics
-      // sendGATimingEvent('end-to-end', 'update', fileType, gaStartTime, Date.now());
+      sendGATimingEvent('end-to-end', 'update', fileType, gaStartTime, Date.now());
     })
     .catch(error => {
       logger.info("update/index.js fail");
